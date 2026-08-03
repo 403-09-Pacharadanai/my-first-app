39f88d9ef007bfc344e2aaa0dfce52d34d3048a6
403-09-Pacharadanai <54383@yupparaj.ac.th>
2026-08-03 13:19:29 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -5,5 +5,5 @@ vat = price * 0.07
 net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
-
-st.divider("นายพชรดนัย อุ่นเรือน เลขที่ 9 ม.4/3")
+st.divider()
+st.write("นายพชรดนัย อุ่นเรือน เลขที่ 9 ม.4/3")
